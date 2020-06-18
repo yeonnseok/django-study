3708d3aa1d860595f2d6daa3e84f8df3ae41427d
yeonnseok <ljinsk3@gmail.com>
2020-06-18 21:58:38 +0900

    feat: index 페이지 생성

--- store/models.py
+++ store/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=20, blank=False, null=False)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=30, blank=False, null=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="book")
+    stock_quantity = models.IntegerField()
+
+
+
