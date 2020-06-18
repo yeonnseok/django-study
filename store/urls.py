64136c7e94aa6ccf517336d97b01177d29ea7ad7
yeonnseok <ljinsk3@gmail.com>
2020-06-18 23:17:22 +0900

    feat: Author 생성

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from store.views import create
+from store.views import create_author, list_author, create_book, list_book
 
 urlpatterns = [
-    path('books/', create, name="create")
+    path('author/create/', create_author, name="create-author"),
+    path('author/list/', list_author, name="list-author"),
+    path('books/create/', create_book, name="create-book"),
+    path('books/list/', list_book, name="list-book"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-
 from store.models import Book, Author
 
 
@@ -7,7 +6,19 @@ def index(request):
     return render(request, "store/index.html")
 
 
-def create(request):
+def create_author(request):
+    if request.method == 'POST':
+        Author.objects.create(name=request.POST['name'])
+        return redirect("/")
+    return render(request, "store/author-form.html")
+
+
+def list_author(request):
+    authors = Author.objects.all()
+    return render(request, "store/author-list.html", {"authors": authors})
+
+
+def create_book(request):
     if request.method == 'POST':
         title = request.POST['title']
         author_id = request.POST['author']
@@ -16,3 +27,8 @@ def create(request):
         return redirect("/")
     authors = Author.objects.all()
     return render(request, "store/book-form.html", {"authors": authors})
+
+
+def list_book(request):
+    books = Book.objects.all()
+    return render(request, "store/book-list.html", {"books": books})
